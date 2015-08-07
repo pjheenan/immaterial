4d65402f712b8a0728bed456a18bf3d1a5ca5275
phillip.heenan <phillip.heenan@vaigogo.com>
2015-08-07 19:14:57 +0100

    Prototype MovieListCache implementation
    * Provide a simple Observable when called
    * Initial setup of DI

--- cache/src/main/java/com/philheenan/immaterial/cache/CacheModule.java
+++ cache/src/main/java/com/philheenan/immaterial/cache/CacheModule.java
@@ -1,7 +1,22 @@
 package com.philheenan.immaterial.cache;
 
+import com.philheenan.immaterial.cache.movie_list.MovieListCache;
+import com.philheenan.immaterial.conductor.facets.movie_list.MovieListCacheFacet;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * @author Phil Heenan on 07/08/15.
  */
+@Module(
+        library = true,
+        complete = false
+)
 public class CacheModule {
+
+    @Provides
+    MovieListCacheFacet provideMovieListCache() {
+        return new MovieListCache();
+    }
 }
