dc38488167d106841c587bb855dae9827f9d8744
phillip.heenan <phillip.heenan@vaigogo.com>
2015-08-07 08:34:22 +0100

    fill out build configuration
    include jacoco code coverage and specify java sdk to avoid bad class magic compile failure

--- core/src/main/java/com/philheenan/immaterial/lib/contract/Facet.java
+++ core/src/main/java/com/philheenan/immaterial/lib/contract/Facet.java
@@ -4,16 +4,16 @@ import rx.Observable;
 
 /**
  * A facet is a class that will provide a self contained access point to a piece of functionality. Examples
- * of `facets` would be 'remote', 'cache', and 'logic'. A Facet will operate on a request/response model 
+ * of `facets` would be 'remote', 'cache', and 'logic'. A Facet will operate on a request/response model
  * with the response passed through an {rx.Observable} implementation.
- * 
+ * <p>
  * `I` is the input to the facet.
  * `O` being the class of object that will be published through the {rx.Observable} implementation.
  *
  * @author Phil Heenan on 06/08/15.
  */
-public interface Facet<I,O> {
+public interface Facet<I, O> {
 
     Observable<O> process(I input);
-    
+
 }
