f439ad0ff2f1d09aac64eaf43b69185c25b4597a
phillip.heenan <phillip.heenan@vaigogo.com>
2015-08-25 09:41:07 +0100

    more MovieList reference cleaning

--- conductor/src/main/java/com/philheenan/immaterial/conductor/facets/movie_list/MovieListCacheFacet.java
+++
@@ -1,9 +0,0 @@
-package com.philheenan.immaterial.conductor.facets.movie_list;
-
-import com.philheenan.immaterial.lib.contract.Facet;
-
-/**
- * @author Phil Heenan on 07/08/15.
- */
-public interface MovieListCacheFacet extends Facet<MovieListCacheRequest, Object> {
-}
